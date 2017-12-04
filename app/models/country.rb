b41cdecd403dbf1bf240e6a1020e79e55521ecb4
Oski <github email address>
2017-12-04 13:04:50 -0500

    @@matches is not triggering when empty

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,7 +2,7 @@ class Country < ActiveRecord::Base
     has_many :user_countries
     has_many :users, through: :user_countries
     validates :name, presence: true, uniqueness: { case_sensitive: false }
-
+    
     @@matches = []
     @@mis = []
 
@@ -31,6 +31,13 @@ class Country < ActiveRecord::Base
     def self.matches?(country)
         @api_countries ||= Country.populate_data_from_api
         @api_countries.map{|country| country["name"]}.any?{|api| api.include?(country)}       
+    end
+
+    def self.create_flag_country_hash
+        country_flags = {}
+        Country.filter
+        Country.matches.map{|country| country_flags[:"#{country}"] = "public/img/flags/#{country}.svg"}
+        binding.pry
     end
     
 end
