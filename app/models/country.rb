7483cb1ed84b19a5fa9a76ee1747aef57be27f4d
Oski <github email address>
2017-12-04 16:54:39 -0500

    Finished functinality for mapping matched countries with flags

--- app/models/country.rb
+++ app/models/country.rb
@@ -7,16 +7,16 @@ class Country < ActiveRecord::Base
     @@mis = []
 
     def self.matches
-        @@matches
+        @@matches.empty? ? Country.worker : @@matches
     end
 
     def self.mis
         @@mis
     end
 
-    def self.populate_data_from_api
-        path = "https://restcountries.eu/rest/v2/all"
-        data = JSON.parse(RestClient.get(path, headers={}))
+    def self.create_flag_country_hash
+        country_flags = {}
+        Country.matches.map{|country_name| country_flags[:"#{country_name}"] = "public/img/flags/#{country_name}.svg"} 
     end
 
     def self.country_names
@@ -24,20 +24,28 @@ class Country < ActiveRecord::Base
         flags ||= Dir.glob("#{path}*.svg").collect{|flag| flag.gsub("#{path}","").gsub(".svg", "")}
     end
 
+    private
+
+    def self.populate_data_from_api
+        path = "https://restcountries.eu/rest/v2/all"
+        data = JSON.parse(RestClient.get(path, headers={}))
+    end
+    
+
     def self.filter
         Country.country_names.each{|country| Country.matches?(country) ? @@matches << country : @@mis << country}       
     end
+    
+
+    def self.worker
+        Country.filter
+        @@matches
+    end
+    
 
     def self.matches?(country)
         @api_countries ||= Country.populate_data_from_api
         @api_countries.map{|country| country["name"]}.any?{|api| api.include?(country)}       
-    end
-
-    def self.create_flag_country_hash
-        country_flags = {}
-        Country.filter
-        Country.matches.map{|country| country_flags[:"#{country}"] = "public/img/flags/#{country}.svg"}
-        binding.pry
     end
     
 end
