788ee78c57b94ec9bdf98f2af9e343736c33d217
Oski <github email address>
2017-12-04 12:11:08 -0500

    Added filtering to check and see if any of the local flage images match the countries coming in from the api call. Currently there is 49 countries that were not matched. Some are named completely different, others simply do not exist

--- app/controllers/country_controller.rb
+++ app/controllers/country_controller.rb
@@ -2,7 +2,7 @@ class CountryController < ApplicationController
 
     get '/countries' do
         if logged_in?
-            @flags ||= Country.countries
+            @flags ||= Country.country_names
             erb :"country/countries"
         else
             redirect '/login'
--- app/models/country.rb
+++ app/models/country.rb
@@ -3,18 +3,34 @@ class Country < ActiveRecord::Base
     has_many :users, through: :user_countries
     validates :name, presence: true, uniqueness: { case_sensitive: false }
 
+    @@matches = []
+    @@mis = []
 
-    def self.create_from_api(name)
-        path = "https://restcountries.eu/rest/v2/all"
-        data = JSON.parse(RestClient.get(path, headers={}))
-        binding.pry
+    def self.matches
+        @@matches
     end
 
-    def self.countries
+    def self.mis
+        @@mis
+    end
+
+    def self.populate_data_from_api
+        path = "https://restcountries.eu/rest/v2/all"
+        data = JSON.parse(RestClient.get(path, headers={}))
+    end
+
+    def self.country_names
         path = "public/img/flags/"
-        @flags ||= Dir.glob("#{path}*.svg").collect do |flag|
-            flag.gsub("#{path}","").gsub(".svg", "")
-        end
+        flags ||= Dir.glob("#{path}*.svg").collect{|flag| flag.gsub("#{path}","").gsub(".svg", "")}
+    end
+
+    def self.filter
+        Country.country_names.each{|country| Country.matches?(country) ? @@matches << country : @@mis << country}       
+    end
+
+    def self.matches?(country)
+        @api_countries ||= Country.populate_data_from_api
+        @api_countries.map{|country| country["name"]}.any?{|api| api.include?(country)}       
     end
     
 end
